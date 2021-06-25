d31d7e15e09d7cab5ea5fb214ac62e5b11b94872
Emilian Roman <roman.emilian@outlook.com>
2021-06-25 14:12:47 +0800

    Implement rudimentary process execution

--- src/Program.cs
+++ src/Program.cs
@@ -17,12 +17,30 @@
  * along with Elevator.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+using Mono.Options;
+using static System.Console;
+using static System.Diagnostics.Process;
+
 namespace Elevator
 {
 	public static class Program
 	{
+		public static readonly OptionSet OptionSet = new OptionSet
+		{
+			{
+				"e|executable=", "executable to invoke with elevated privileges",
+				s => Executable = s
+			}
+		};
+
+		private static string Executable { get; set; } = string.Empty;
+
 		public static void Main(string[] args)
 		{
+			OptionSet.WriteOptionDescriptions(Out);
+			OptionSet.Parse(args);
+
+			Start(Executable);
 		}
 	}
 }
